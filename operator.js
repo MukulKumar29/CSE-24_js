7a6f8db8dc26e20acf530369785f73b7e0ea4dbc
MukulKumar29 <mukulchaudhary8725@gmail.com>
2026-02-24 22:01:42 +0530

    operators

---
+++ operator.js
@@ -0,0 +1,102 @@
+// Arithmatic operator
+            var a=100; var b=13; var c=10; var linebreak="<br>";
+
+            console.log("a+b+c=");
+            result=a+b+c;
+            console.log(result);
+            console.log(linebreak);
+
+            console.log("a+b=");
+            result=a+b;
+            console.log(result);
+            console.log(linebreak);
+
+            console.log("a-b=");
+            result=a-b;
+            console.log(result);
+            console.log(linebreak);
+
+            console.log("a/b=");
+            result=a/b;
+            console.log(result);
+            console.log(linebreak);
+
+            console.log("a%b=");
+            result=a%b;
+            console.log(result);
+            console.log(linebreak);
+
+      
+// Comparison operator
+    
+var a = 10;
+ var b = 20;
+ var linebreak="<br/>";
+
+ console.log("(a == b) => ");
+ result = (a == b);
+ console.log(result);
+ console.log(linebreak);
+
+ console.log("(a < b) => ");
+ result = (a < b);
+ console.log(result);
+ console.log(linebreak);
+
+ console.log("(a > b) => ");
+ result = (a > b);
+ console.log(result);
+ console.log(linebreak);
+
+ console.log("(a != b) => ");
+ result = (a != b);
+ console.log(result);
+ console.log(linebreak);
+
+ console.log("(a >= b) => ");
+ result = (a >= b);
+ console.log(result);
+ console.log(linebreak);
+
+ console.log("(a <= b) => ");
+ result = (a <= b);
+ console.log(result);
+ console.log(linebreak);
+ 
+ // Assignment Operator
+
+
+ var a = 40;
+ var b = 20;
+ var linebreak = "<br />";
+ 
+ console.log("Value of a => (a = b) => ");
+ result = (a = b);
+ console.log(result);
+ console.log(linebreak);
+ 
+ console.log("Value of a => (a += b) => ");
+ result = (a += b);
+ console.log(result);
+ console.log(linebreak);
+ 
+ console.log("Value of a => (a -= b) => ");
+ result = (a -= b);
+ console.log(result);
+ console.log(linebreak);
+ 
+ console.log("Value of a => (a *= b) => ");
+ result = (a *= b);
+ console.log(result);
+ console.log(linebreak);
+ 
+ console.log("Value of a => (a /= b) => ");
+ result = (a /= b);
+console.log(result);
+ console.log(linebreak);
+ 
+ console.log("Value of a => (a %= b) => ");
+ result = (a %= b);
+ console.log(result);
+ console.log(linebreak);
+ 
