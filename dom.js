555faba275be22815400544d7c9551e720986892
MukulKumar29 <mukulchaudhary8725@gmail.com>
2026-04-24 21:40:52 +0530

    commit dom

---
+++ dom.js
@@ -0,0 +1,34 @@
+// javascript  can change all the html elements in the page
+// java can change all html attributes in the page
+// and all the css in the page
+//java can remove all c and attributes
+// java can react to the exixting html events
+// javas. can create new html evrnts in the page 
+
+let x=document.getElementById("demo").getAttribute("demo");
+x.innerHTML=" ";
+let y=document.getElementsByClassName("demo,memo");
+y[0].innerHTML=" ";
+y[1].innerHTML=" ";
+let z=document.getElementsByTagName("demo");
+document.querySelector("  ");
+document.querySelectorAll(  );
+let x=document.getElementById("demo").setAttribute("class","democlass");
+//styling
+let x=document.getElementById("demo").style.color="blue";
+//Create an element
+let y=document.createElement("h2");
+let par=document.getElementsByTagName("body");
+// mouse events 
+<button onClick="onclick()">onclick</button>;
+<button onMouseover={}>onmouseover</button>;
+<button onMouseevent={}>onmouseover</button>;
+<button onMouseDown={}>onMousedown</button>;
+<button onMouseup={}>onMouseup</button>;
+<button onMousemove={}>onMousemove</button>;
+console.log("buttons")
+//
+<button onkeydown={}>onkeydown</button>;
+<button onkeyfoucs={}>onkeydown</button>;
+<button onKeyUp={}>onkeyUp</button>;
+<button onunload={}>onkeydown</button>;
