6e6f9bc09ba19712c0ef5b08013f44780251ad63
MukulKumar29 <mukulchaudhary8725@gmail.com>
2026-04-24 21:43:06 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,10 @@
+function myfunction()
+{
+    alert("Hello World");
+}
+// arrow function is an anonomus function -> function in an variable
+// arrow function start (x,y)=>{return x+y}
+//objects is repersented by curly brackets
+
+let add =(x,y) =>{console.log(x+y)};
+add(4,5)
