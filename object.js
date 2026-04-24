ae0bcbdb79595df7adc76d0df9452a218610c48a
MukulKumar29 <mukulchaudhary8725@gmail.com>
2026-04-24 21:46:04 +0530

    objects

---
+++ object.js
@@ -0,0 +1,11 @@
+
+
+var person2=new Object();
+person2.name="krish";
+person2.age=32;
+person2.isMarried=true;
+person2.address={};
+person2.address.street="nagar road";
+person2['address']['flatNo()']=33;
+console.log(person)
+
