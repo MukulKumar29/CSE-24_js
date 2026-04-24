db7d7848c2d84e21bbebd984613e2bef259d41e7
MukulKumar29 <mukulchaudhary8725@gmail.com>
2026-04-24 21:45:26 +0530

    main

---
+++ main.js
@@ -0,0 +1,8 @@
+var grade='A';
+document.write("entering switch block<br/>");
+switch(grade){
+    case'A':document.write("good job<br/>");
+    break;
+     case'A':document.write("good job<br/>");
+    break;
+}
